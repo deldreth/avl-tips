dafbe38e3b8eefc5213a9f163b6549a58925a288
Devin Eldreth <devin.eldreth@gmail.com>
2020-03-19 00:40:19 -0400

    chore: cleanup, favicon, image
    Co-authored-by: Chris McGee <krystophv@gmail.com>

--- packages/client/src/App.js
+++ packages/client/src/App.js
@@ -28,21 +28,19 @@ function App() {
             <div className="column is-mobile is-two-thirds-tablet is-half-desktop">
               <div className="card has-background-grey-dark has-text-light">
                 <div className="card-content">
-                  <h1 className="title has-text-light is-spaced">
+                  <p className="title has-text-light is-size-6-mobile">
                     Help the Asheville Service Industry
-                  </h1>
-                  <h2 className="subtitle has-text-light">
+                  </p>
+                  <p className="subtitle has-text-light is-size-6-mobile">
                     Every time you have a drink at home during social
                     distancing, consider tipping a local service industry worker
                     through Venmo or Cash App. Refresh this page to get a new
-                    person to support.
-                  </h2>
-                  <h2 className="subtitle has-text-light">
-                    Right now, service industry people are severely impacted by
-                    social distancing and quarantine. Lower amounts of patrons
-                    and restaurants closing will be tough on everyone. Every
-                    little bit helps.
-                  </h2>
+                    person to support. Right now, service industry people are
+                    severely impacted by social distancing and quarantine. Lower
+                    amounts of patrons and restaurants closing will be tough on
+                    everyone. Every little bit helps.
+                  </p>
+                  <p className="subtitle has-text-light"></p>
                   <p>
                     If you are a service industry worker impacted by this in
                     Asheville,{" "}
@@ -77,7 +75,7 @@ function App() {
           Image: mogmismo
         </a>{" "}
         |{" "}
-        <a href="mailto:" className="has-text-white">
+        <a href="mailto:inquiries@avl.tips" className="has-text-white">
           Questions and Data Privacy Inquiries
         </a>
       </footer>
--- packages/client/src/Error/Error.js
+++ packages/client/src/Error/Error.js
@@ -2,11 +2,11 @@ import React from "react";
 
 function Error(props) {
   return (
-    <article class="message is-warning">
-      <div class="message-header">
+    <article className="message is-warning">
+      <div className="message-header">
         <p>Something went wrong</p>
       </div>
-      <div class="message-body">
+      <div className="message-body">
         Either there are no participants yet or something is wrong.
       </div>
     </article>
